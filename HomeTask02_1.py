cb08494a9458e68e567be9715c2c9e8cc0205f05
Кириченко Олег Геннадійович <olegg.kyrychenko@fora.ua>
2024-12-24 12:13:24 +0200

    HomeTask 2.1

---
+++ HomeTask02_1.py
@@ -0,0 +1,11 @@
+print("Hello, my dear friend!")
+four_numbers = input("Could you write fore numbers, please? ")
+a = int(four_numbers) // 1000
+b = int(four_numbers) % 1000 // 100
+c = int(four_numbers) % 100 // 10
+d = int(four_numbers) % 10
+
+print(a)
+print(b)
+print(c)
+print(d)
