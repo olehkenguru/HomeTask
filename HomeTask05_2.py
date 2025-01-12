2659ee4b3a3d3fbe7e22a1201db8da8fcc5cdabc
olegg.kyrychenko <olega.asamit@gmail.com>
2025-01-12 14:49:20 +0200

    Add HomeTask05_2.py

---
+++ HomeTask05_2.py
@@ -0,0 +1,28 @@
+print("****** Power Calculator ******")
+
+while(True):
+
+    number_first = float(input("Enter the firs number: "))
+    number_second = float(input("Enter the second number: "))
+    action_numbers = input("Enter the operation on numbers (+, -, *, /): ")
+    result = None
+
+    match action_numbers:
+        case "+":
+            result = number_first + number_second
+        case "-":
+            result = number_first - number_second
+        case "*":
+            result = number_first * number_second
+        case "/" if number_second != 0:
+            result = number_first / number_second
+        case "/" if number_second == 0:
+            result = "You can't divide by zero."
+        case _:
+            result = "Something wrong."
+
+
+    print(f"Result: {number_first} {action_numbers} {number_second} = {result}")
+
+    if input('Do you want to continue? ').strip().lower() not in {'y', 'yes'}:
+        break
