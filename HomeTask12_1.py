d18747c82486f496286e5e54aef1c04177b1c528
olegg.kyrychenko <olega.asamit@gmail.com>
2025-02-09 18:11:16 +0200

    Fix HomeTask12_1.py

--- HomeTask12_1.py
+++ HomeTask12_1.py
@@ -3,25 +3,24 @@ import codecs
 def delete_html_tags(html_file, result_file='cleaned.txt'):
     with codecs.open(html_file, 'r', 'utf-8') as file:
         html = file.read()
-        clean_text = ''
-        html_teg = False
 
-        for char in html:
-            if char == '<':
-                html_teg = True
-            elif char == '>':
-                html_teg = False
-                continue
+    clean_text = []
+    html_tag = False
 
-            if not html_teg:
-                clean_text += char
+    for char in html:
+        if char == '<':
+            html_tag = True
+        elif char == '>':
+            html_tag = False
+            continue
+        if not html_tag:
+            clean_text.append(char)
 
-    with codecs.open(result_file, 'w', 'utf-8') as clean_file:
-        for i in clean_text.split('\n'):
-            if i.strip():
-                clean_file.writelines(i.strip() + '\n')
+    clean_text = ''.join(clean_text)
+    lines = [line.strip() for line in clean_text.splitlines() if line.strip()]
 
-    return
+    with open(result_file, 'w', encoding='utf-8') as clean_file:
+        clean_file.write('\n'.join(lines) + '\n')
 
 
 delete_html_tags('draft.html')
