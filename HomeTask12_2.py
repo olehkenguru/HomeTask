25fb265fe1b07601be687a274b8b7ae275c7cacc
olegg.kyrychenko <olega.asamit@gmail.com>
2025-02-09 12:12:26 +0200

    Add HomeTask12_2.py

---
+++ HomeTask12_2.py
@@ -0,0 +1,80 @@
+class Item:
+
+    def __init__(self, name, price, description, dimensions):
+        self.price = price
+        self.description = description
+        self.dimensions = dimensions
+        self.name = name
+
+    def __str__(self):
+        return f'{self.name}, price: {self.price}'
+
+
+class User:
+
+    def __init__(self, name, surname, numberphone):
+        self.name = name
+        self.surname = surname
+        self.numberphone = numberphone
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
+
+class Purchase:
+    def __init__(self, user):
+        self.products = {}
+        self.user = user
+        self.total = 0
+
+    def add_item(self, item, cnt):
+        self.products[item] = cnt
+
+    def __str__(self):
+        description_cart =  f"User: {str(self.user)}\nItems:\n"
+        for item in self.products:
+            description_cart += f'{item.name}, {self.products[item]} pcs.\n'
+        return description_cart[:len(description_cart)-1]
+
+    def get_total(self):
+        return sum(item.price * self.products[item] for item in self.products)
+
+
+# cellphone_motorola = Item('Motorola', 30.0, 'old school cellphone', 'small')
+# laptop_apple = Item('Macbook', 500.0, 'nice but expensive technique ', 'big')
+#
+# # print(cellphone_motorola)
+# # print(laptop_apple)
+#
+# buyer_1 = User('Vasia', 'Pupkin', '+380671122333')
+# buyer_2 = User('Dart', 'Weider', '+380800100102')
+#
+# # print(buyer_1)
+# # print(buyer_2)
+#
+# cart = Purchase(buyer_1)
+# cart.add_item(cellphone_motorola, 2)
+# cart.add_item(laptop_apple, 4)
+#
+# print(cart)
+#
+# Example from task
+
+lemon = Item('lemon', 5, "yellow", "small", )
+apple = Item('apple', 2, "red", "middle", )
+print(lemon)  # lemon, price: 5
+
+buyer = User("Ivan", "Ivanov", "02628162")
+print(buyer)  # Ivan Ivanov
+
+cart = Purchase(buyer)
+cart.add_item(lemon, 4)
+cart.add_item(apple, 20)
+print(cart)
+
+assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
+assert cart.get_total() == 60, "Всього 60"
+assert cart.get_total() == 60, 'Повинно залишатися 60!'
+cart.add_item(apple, 10)
+print(cart)
+assert cart.get_total() == 40
