2af11b941e9d80e6b474f95b7753a1a42adddfe1
olegg.kyrychenko <olega.asamit@gmail.com>
2025-02-09 08:08:13 +0200

    Add HomeTask12_1.py

---
+++ HomeTask12_1.py
@@ -0,0 +1,27 @@
+import codecs
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html = file.read()
+        clean_text = ''
+        html_teg = False
+
+        for char in html:
+            if char == '<':
+                html_teg = True
+            elif char == '>':
+                html_teg = False
+                continue
+
+            if not html_teg:
+                clean_text += char
+
+    with codecs.open(result_file, 'w', 'utf-8') as clean_file:
+        for i in clean_text.split('\n'):
+            if i.strip():
+                clean_file.writelines(i.strip() + '\n')
+
+        return
+
+
+delete_html_tags('draft.html')
